8c7cb660e24d1deb20732cfda97572e35e43e3cb
eevlogieva <eevlogieva@yahoo.com>
2014-10-11 14:53:12 +0300

    Tasks for week 0

---
+++ Problem_set_2/is_member_fib_lists.py
@@ -0,0 +1,30 @@
+from fibonacci_lists import nth_fib_lists
+
+
+def fib_random_numbers(a, b):
+    result = []
+    for index in range(0, 10):
+        if index == 0:
+            result.append(a)
+        elif index == 1:
+            result.append(b)
+        else:
+            result.append(result[index - 1] + result[index - 2])
+    return result
+#print(fib_random_numbers(3, 2))
+
+
+def member_of_nth_fib_lists(listA, listB, needle):
+    lst = fib_random_numbers(len(listA), len(listB))
+    print(lst)
+    if len(needle) not in lst:
+        print("first here")
+        return False
+    else:
+        for item in needle:
+            if (item not in listA) and (item not in listB):
+                print("here ")
+                return False
+        print(nth_fib_lists(listA, listB, lst.index(len(needle)) + 1))
+        return needle == nth_fib_lists(listA, listB, lst.index(len(needle)) + 1)
+print(member_of_nth_fib_lists([1, 2], [3, 4], [1, 2, 3, 4, 3, 4, 1, 2, 3, 4]))
